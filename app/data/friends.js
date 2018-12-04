044375d852ee3a004701ef5746dca3b3c817f83b
Chris Quinones <chrisquinones@Christophers-MacBook.local>
2018-12-04 00:08:20 -0600

    p1

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friend = [
+    {
+        name: "abc",
+        photo: "http://4.bp.blogspot.com/-iZQMpLAhx0Q/VPC_P9sfI9I/AAAAAAAAGGQ/bR2s8AkZZxc/s1600/natural-beauty_switzerland-2c.jpg",
+        score: [
+            "1",
+            "3",
+            "4",
+            "4",
+            "5",
+            "1",
+            "3",
+            "5",
+            "2",
+            "4"
+        ]
+    }
+]
+
+module.exports = friend;
