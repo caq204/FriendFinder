0625801dbe2ec485f260c4810da341ee2eea8edc
Chris Quinones <chrisquinones@Christophers-MacBook.local>
2018-12-04 20:13:01 -0600

    p2

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friend = [
     {
-        name: "abc",
-        photo: "http://4.bp.blogspot.com/-iZQMpLAhx0Q/VPC_P9sfI9I/AAAAAAAAGGQ/bR2s8AkZZxc/s1600/natural-beauty_switzerland-2c.jpg",
+        name: "Darth Mual/Maul",
+        photo: "http://la-saga-star-wars.allmyblog.com/images/la-saga-star-wars/la-saga-star-wars_20070331_145138.jpg",
         score: [
             "1",
             "3",
@@ -14,7 +14,153 @@ var friend = [
             "2",
             "4"
         ]
-    }
-]
+    },
+
+    {
+		name: "Broly",
+		photo: "https://i.pinimg.com/originals/92/72/a4/9272a413156ca1144eab589110672e78.png",
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Spike",
+		photo: "https://images-na.ssl-images-amazon.com/images/I/41K7FzpBQXL._SX425_.jpg",
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: "Wade",
+		photo: "https://cdn.shopify.com/s/files/1/0584/3841/products/100-polyester-marvel-deadpool-riding-a-unicorn-plush-1_2048x.jpg?v=1524869490",
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: "Sid",
+		photo: "https://i.ytimg.com/vi/F1bSg6ajDGI/hqdefault.jpg",
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: "Doc Ock",
+		photo: "https://cdn.shopify.com/s/files/1/0580/0965/products/72dpi-Chunkies_Doc_Ock.jpg?v=1507754726",
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Mr Ricky",
+		photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU0NzcxODI4MjM5/rick-james-248967-1-402.jpg",
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: "Vanilla Ice",
+		photo: "https://pbs.twimg.com/media/DOZqxlVX0AAEPeC.jpg",
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Project",
+		photo: "https://www.celebswealth.com/wp-content/uploads/2018/05/pat.jpg",
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: "The Don",
+		photo: "https://cdn.cnn.com/cnnnext/dam/assets/160115151441-donald-trump-shrug-super-tease.jpg",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
 
 module.exports = friend;
